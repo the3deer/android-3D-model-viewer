bb660d3dbb131fa584816570380681f41712f532
Andres Oviedo <andresoviedo@gmail.com>
2020-10-03 22:14:58 +0200

    new skinId attribute

--- engine/src/main/java/org/andresoviedo/android_3d_model_engine/services/collada/entities/SkinningData.java
+++ engine/src/main/java/org/andresoviedo/android_3d_model_engine/services/collada/entities/SkinningData.java
@@ -4,19 +4,21 @@ import java.util.List;
 
 public class SkinningData {
 
+	private final String id;
     private final float[] bindShapeMatrix;
 	public final List<String> jointOrder;
 	public final List<VertexSkinData> verticesSkinData;
 	private final float[] inverseBindMatrix;
 
 	/**
-	 *
+	 *  @param skinId
 	 * @param bindShapeMatrix bind_shape_matrix or IDENTITY_MATRIX
 	 * @param jointOrder
 	 * @param verticesSkinData
 	 * @param inverseBindMatrix optional value
 	 */
-	public SkinningData(float[] bindShapeMatrix, List<String> jointOrder, List<VertexSkinData> verticesSkinData, float[] inverseBindMatrix){
+	public SkinningData(String skinId, float[] bindShapeMatrix, List<String> jointOrder, List<VertexSkinData> verticesSkinData, float[] inverseBindMatrix){
+		this.id = skinId;
 	    this.bindShapeMatrix = bindShapeMatrix;
 		this.jointOrder = jointOrder;
 		this.verticesSkinData = verticesSkinData;
@@ -30,5 +32,9 @@ public class SkinningData {
 
 	public float[] getInverseBindMatrix() {
 		return inverseBindMatrix;
+	}
+
+	public String getId() {
+		return this.id;
 	}
 }
