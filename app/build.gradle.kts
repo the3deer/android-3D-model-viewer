f5c39fb2bf0eb0661d1b6ea9c719a3e6cb05692f
Andrés Oviedo <andresoviedo@gmail.com>
2026-04-16 22:02:27 +0200

    fixed version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.the3deer.android.viewer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "5.0.1"
+        versionCode = 6
+        versionName = "5.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
