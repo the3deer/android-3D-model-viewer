616ae5cb661bff926123bd1106da353f2d151cb1
Andrés <git@andresoviedo.dev>
2026-04-22 22:19:31 +0200

    zip is now cached

--- app/src/main/java/org/the3deer/android/viewer/ui/load/LoadContentDialog.java
+++ app/src/main/java/org/the3deer/android/viewer/ui/load/LoadContentDialog.java
@@ -92,14 +92,13 @@ public class LoadContentDialog {
     public void load(URI uri) throws IOException, InterruptedException {
 
         // detect model type
-        // example: content://com.google.android.apps.docs.storage/document/acc%3D1%3Bdoc%3Dencoded%3Dv5Vt6bpRbbWqAplsgXWSPOVnJa1cmWj2SQIwTejAt2kl2xistjSRKLP4S-s%3D
-        final String fileName = ContentUtils.getFileName(getActivity().getApplicationContext(), URI.create(uri.toString()));
+        final String fileName = ContentUtils.getFileName(getActivity().getApplicationContext(), uri);
         final String fileType = fileName != null && fileName.contains(".") ? fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase() : "unsupported";
 
         // check
         if (fileName == null) throw new IllegalArgumentException("No filename found for " + uri);
 
-        // check if zip
+        // check extension
         switch (fileType) {
             case "obj":
             case "stl":
@@ -107,67 +106,8 @@ public class LoadContentDialog {
             case "gltf":
             case "glb":
             case "fbx":
-                launchFragment(uri, fileName, fileType);
-                break;
             case "zip":
-
-                // sanitize filename
-                final String fileNameSanitized = fileName.replaceAll(" ", "+");
-
-                // create uri
-                final URI dataUri = URI.create("android://" + getActivity().getPackageName() + "/binary/" + fileNameSanitized);
-
-                // register resource
-                ContentUtils.addUri(fileName, dataUri);
-
-                // FIXME: potential out of memory error
-                final Map<String, byte[]> zipFiles = ContentUtils.readFiles(URI.create(uri.toString()));
-                URI modelFile = null;
-                String extension = null;
-                for (Map.Entry<String, byte[]> zipFile : zipFiles.entrySet()) {
-
-                    final String zipFilename = zipFile.getKey();
-                    if (zipFilename.startsWith("__MACOSX") || zipFilename.contains("/._") || zipFilename.startsWith("._")) {
-                        continue;
-                    }
-
-                    final int dotIndex = zipFilename.lastIndexOf('.');
-                    final String fileExtension;
-                    if (dotIndex != -1) {
-                        fileExtension = zipFilename.substring(dotIndex).toLowerCase();
-                    } else {
-                        continue; // it's probably a folder. ignore it
-                    }
-
-                    // sanitize filename
-                    final String fileNameEntrySanitized = zipFilename.replaceAll(" ", "+");
-
-                    // build uri
-                    final URI pseudoUri = dataUri.resolve(fileNameEntrySanitized);
-
-                    // register all zip entries
-                    ContentUtils.addUri(fileNameEntrySanitized, pseudoUri);
-                    ContentUtils.addData(pseudoUri, zipFile.getValue());
-
-                    // detect model
-                    switch (fileExtension) {
-                        case ".obj":
-                        case ".stl":
-                        case ".dae":
-                        case ".gltf":
-                        case ".fbx":
-                        case ".glb":
-                            modelFile = pseudoUri;
-                            logger.info("Found model in zip:");
-                            extension = fileExtension.substring(1);
-                            break;
-                    }
-                }
-                if (modelFile != null) {
-                    launchFragment(modelFile, fileName, extension);
-                } else {
-                    throw new IllegalArgumentException("No model found in zip file: " + uri);
-                }
+                launchFragment(uri, fileName, fileType);
                 break;
             default:
                 throw new IllegalArgumentException("Unknown extension: " + fileName +
